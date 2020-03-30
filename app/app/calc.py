82c3b3dc1e7f65b09e1669ce5ca7e987e0f985c2
Thomas Schild <thomas.schild.ch@gmail.com>
2020-03-30 16:27:05 +0200

    added first tdd test and extended calc with subtract function

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,3 +1,7 @@
-def add(x, y):
+def add(a, b):
     """add two numbers and return result"""
-    return x + y;
+    return a + b;
+
+def subtract(a, b):
+    """subtract a from b and return result"""
+    return b - a;
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,9 +1,12 @@
 from django.test import TestCase
-from app.calc import add
+from app.calc import add, subtract
 
 class CalcTests(TestCase):
 
     def test_add_numbers(self):
-        """test add() function, two numbers are added correctly"""
+        """test add(a,b) function, numbers a and b are added correctly"""
         self.assertEqual(add(3, 8), 11);
 
+    def test_subtract_numbers(self):
+        """test subtract(a,b) function, where a gets subtracted from b and assert result is correct"""
+        self.assertEqual(subtract(3, 11), 8);
