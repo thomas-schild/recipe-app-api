a708a6d5bc2c97598d70b867cd817ce55d550770
Thomas Schild <thomas.schild.ch@gmail.com>
2020-04-01 15:07:23 +0200

    implemeneted UserModel

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
-        """Test creating a user with an email is success"""
+        """Test creating a user with an email is successful"""
         # setup / prepare
         testEmail = 'dummy.user@demo.org'
         testPasswd = 'dummy-secret-pw'
