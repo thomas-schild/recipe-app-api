bb5cf7248cbe2ce7ddfb600d57d3effe4acf8341
Thomas Schild <thomas.schild.ch@gmail.com>
2020-04-01 10:55:11 +0200

    added test_models incore; test to create user, tdd, so no impl yet

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a user with an email is success"""
+        # setup / prepare
+        testEmail = 'dummy.user@demo.org'
+        testPasswd = 'dummy-secret-pw'
+        # run
+        user = get_user_model().objects.create_user(
+            email = testEmail,
+            password = testPasswd
+        )
+        # assert
+        self.assertEqual(user.email, testEmail)
+        self.assertTrue(user.check_password(testPasswd))
